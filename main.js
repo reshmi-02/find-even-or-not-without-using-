68f646a9183481204e80cfd66e62022c02db5436
reshmi-02 <reshmihash123@gmail.com>
2024-01-08 16:43:17 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,10 @@
+let num1=parseInt(prompt("Enter the number"));
+document.write("Given num = "+num1+"<br><br>")
+
+if(num1&1==0){
+    document.write(num1+" is even number")
+}
+else{
+    document.write(num1+" is odd number")
+}
+
